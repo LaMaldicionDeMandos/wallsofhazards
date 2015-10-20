70ed139725aae3aed1f52f411dd38541b5cfb7ee
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-10-20 19:25:05 -0300

    agrego log simple

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.sendFile('index.html');
+  	res.sendFile('index.html');
 });
 
 app.post('/', function(req, res) {
+	console.log('post from facbook: ' + JSON.stringify(req.body));
+	console.log('post from facbook: ' + JSON.stringify(req.path));
+	console.log('post from facbook: ' + JSON.stringify(req.params));
+	console.log('post from facbook: ' + JSON.stringify(req.query));
 	res.sendFile('public/index.html', {root: __dirname });
 });
 
