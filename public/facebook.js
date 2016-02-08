61b4574d880a1c7662b52ca37da8f9d15384e9dd
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-02-08 18:56:14 -0300

    log para ver que viene en data

--- public/facebook.js
+++ public/facebook.js
@@ -9,6 +9,7 @@
   		    FB.api('/me?fields=first_name', function(data) {
   		      var welcomeBlock = document.getElementById('fb-welcome');
   		      welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
+            console.log(JSON.stringify(data));
             username = data.first_name;
   		    });
   		  }
