36041cdaca3378080432ca6fcffc7b6898b65ebb
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-02-08 23:21:49 -0300

    llamo a unity en las respuestas

--- public/messages.js
+++ public/messages.js
@@ -5,14 +5,10 @@ function givemeUserName(object, callback) {
 function givemeLevel(object, callback) {
 	getLevel(user.id, function(data) {
 		console.log('givemeLevel: ' + data);
-		callback(data);
-		//SendMessage(object, callback, data);
+		SendMessage(object, callback, data);
 	});
 }
 
 function levelUp(object, callback, level) {
-	sendLevelUp({userId: user.id, level: level}, function(data) { 
-		callback();
-		//SendMessage(object, callback, level + 1);
-	});
+	sendLevelUp({userId: user.id, level: level});
 }
